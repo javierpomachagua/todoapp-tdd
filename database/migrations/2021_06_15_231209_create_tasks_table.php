5465ca87fed955a3c6929b6490542a6db2583217
Javier Pomachagua <javierpp0303@gmail.com>
2021-06-15 18:42:28 -0500

    add some test for relationship between goal and taks

---
+++ database/migrations/2021_06_15_231209_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('goal_id')->constrained()->cascadeOnDelete();
+            $table->string('name');
+            $table->integer('order');
+            $table->boolean('is_finish')->default(0);
+            $table->timestamp('alert_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
--- tests/Feature/ManageTaskTest.php
+++ tests/Feature/ManageTaskTest.php
@@ -2,16 +2,77 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ManageTaskTest extends TestCase
 {
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_an_user_can_has_own_goals()
+    {
+        // User login
+        $user = User::factory()->create();
+
+        $this->actingAs($user, 'api');
+
+        $this->assertAuthenticated('api');
+
+        // Create goal for that user
+        $goal = $user->goals()->create([
+            'name' => 'Lose Weight'            
+        ]);
+
+        // Show goals of user
+        $goals = $user->goals()->count();
+
+        // Compare quantity
+        $this->assertEquals($goals, 1);
     }
+
+    public function test_an_user_cannot_has_other_goals()
+    {
+        // Create user and create goals for that user
+        $firstUser = User::factory()->create();
+
+        $firstUser->goals()->create([
+            'name' => 'Lose Weight'
+        ]);
+
+        // Login with other user and show goals
+        $secondUser = User::factory()->create();        
+
+        $secondUserGoals = $secondUser->goals()->count();
+
+        // Assert that second user cannot see first user goals
+        $this->assertEquals($secondUserGoals, 0);
+    }
+
+    public function test_an_user_can_save_goals()
+    {
+        $this->withoutExceptionHandling();
+        
+        // Login as user
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        // Create a goal for that user requesting an api
+        $response = $this->post('api/goals', [
+            'name' => 'Lose Weight',
+            'user_id' => $user->id
+        ]);
+
+        // Return 201
+        // Return message
+        $response->assertStatus(201)
+            ->assertJson([
+                'message' => 'Goal created!'
+            ]);
+
+    }
+
+    
 }
